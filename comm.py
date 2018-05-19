42059afd128d35888e82fa12762a9f192a39d07c
MaxHerrera <max.herrera@correo.usa.edu.co>
2018-05-19 09:41:22 -0500

    modificacion de puerto

--- comm.py
+++ comm.py
@@ -1,6 +1,6 @@
 import serial
 
-puerto = serial.Serial('/dev/tty.usbmodem14231')
+puerto = serial.Serial('/dev/tty.usbmodem')
 while(True):
     datos = str(serial.Serial.readline(puerto))
     print(datos)
